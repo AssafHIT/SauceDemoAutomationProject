00f8ab4e8fc8ce704211632b94151b39853aba9c
AssafHIT <asafyehezkel237@gmail.com>
2025-02-19 13:41:07 +0200

    Added config.ini and updated login test

--- tests/login_test.py
+++ tests/login_test.py
@@ -1,25 +1,31 @@
 import allure
 import pytest
+from utils.config import ConfigReader
+
 
 @allure.suite("Login Tests")
 class TestLogin:
+    # Using data from config.ini:
+    valid_username = ConfigReader.read_config("login", "username")
+    valid_password = ConfigReader.read_config("login", "password")
+    base_url = ConfigReader.read_config("settings", "base_url")  # Added base_url from config.ini
 
     @pytest.mark.critical
     @pytest.mark.parametrize(
         "username, password, expected_url, expected_error_message, description",
         [
-            ("standard_user", "secret_sauce", "https://www.saucedemo.com/inventory.html", None,
+            (valid_username, valid_password, f"{base_url}/inventory.html", None,
              "Valid Login Test for Standard User"),
-            ("standad_usr", "secret_sauce", None,
+            ("standad_usr", valid_password, None,
              "Epic sadface: Username and password do not match any user in this service",
              "Invalid Username Login Test"),
-            ("standard_user", "secrt_suce", None,
+            (valid_username, "secrt_suce", None,
              "Epic sadface: Username and password do not match any user in this service",
              "Invalid Password Login Test"),
-            ("locked_out_user", "secret_sauce", None, "Epic sadface: Sorry, this user has been locked out.",
+            ("locked_out_user", valid_password, None, "Epic sadface: Sorry, this user has been locked out.",
              "Locked-Out User Login Test"),
-            ("", "secret_sauce", None, "Epic sadface: Username is required", "Login Without Username Test"),
-            ("standard_user", "", None, "Epic sadface: Password is required", "Login Without Password Test"),
+            ("", valid_password, None, "Epic sadface: Username is required", "Login Without Username Test"),
+            (valid_username, "", None, "Epic sadface: Password is required", "Login Without Password Test"),
         ],
     )
     @allure.title("{description}")
@@ -33,5 +39,3 @@ class TestLogin:
             # Invalid login case
             error_message = login_page.get_error_message()
             assert error_message == expected_error_message, f"Expected error message: '{expected_error_message}', but got '{error_message}'."
-
-
